2f39ff19a3788fd9d36648abdbfd6b468e372cd2
Joynewthe <joymajhi597@gmail.com>
2025-03-23 07:46:22 +0600

    config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config(object):
     API_HASH = environ.get("API_HASH", "0db310b3f4e8b07d938bcf2295bcb03d")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7370621547:AAHf59KoPaU5hsnJ7nsOP5Q05-Te8Hj3xIs")
     #The bot will work only with following users
-    AUTH_USERS = list(int(x) for x in environ.get("AUTH_USERS", "6360672597").split(" "))
+    AUTH_USERS = list(int(x) for x in environ.get("AUTH_USERS", "1885207148").split(" "))
     #Owner ID is the user id of your telegram account
-    OWNER_ID = int(environ.get("OWNER_ID", "6360672597"))
+    OWNER_ID = int(environ.get("OWNER_ID", "1885207148"))
     #keep default unless you know what you are doing
     DOWNLOAD_DIRECTORY = environ.get("DOWNLOAD_DIRECTORY","./downloads")
    
